0f0f82a853205012a4676b542cbc4bd9a095485c
milanc <8333458+milanc@users.noreply.github.com>
2024-04-24 23:12:05 -0400

    added blazor app

---
+++ BlazorApp/BlazorApp/Program.cs
@@ -0,0 +1,31 @@
+using BlazorApp.Client.Pages;
+using BlazorApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorApp.Client._Imports).Assembly);
+
+app.Run();
