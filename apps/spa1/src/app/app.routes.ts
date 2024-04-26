258a4e2d28f321114e829e383ff499309d62eb2d
milanc <8333458+milanc@users.noreply.github.com>
2024-04-26 06:30:17 -0400

    adding components

--- apps/spa1/src/app/app.routes.ts
+++ apps/spa1/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Route } from '@angular/router';
+import { Component1Component } from './component1/component1.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: 'component1', component: Component1Component },
+];
--- apps/spa2/src/app/app.routes.ts
+++ apps/spa2/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Route } from '@angular/router';
+import { Component2Component } from './component2/component2.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: 'component2', component: Component2Component },
+];
