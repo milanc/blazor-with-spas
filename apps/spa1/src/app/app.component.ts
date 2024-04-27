d548267d08876843368f88431b765349895bf5e2
milanc <8333458+milanc@users.noreply.github.com>
2024-04-26 22:59:49 -0400

    try

--- apps/spa1/src/app/app.component.ts
+++ apps/spa1/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [NxWelcomeComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
